fa1e44c2fac2607e736326fe71442b85481f5b66
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 04:25:40 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -11,6 +11,6 @@ require 'pry'
 def get_the_min(groceries)
   #code your solution here!
   groceries.each do |grocery|
-    grocery.min_by{|k, v| v}.to_a
-end
+    grocery.min_by{|k, v| v}.to_s
+  end
 end
